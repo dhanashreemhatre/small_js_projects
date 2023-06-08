1d288dabd881bcb77ae50e4fa4c60c90bfb238e8
dhanashreemhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2023-06-08 00:09:51 -0700

    Add files via upload

---
+++ people counter/new.js
@@ -0,0 +1,16 @@
+let countEl=document.getElementById("count-el");
+let saveEl=document.getElementById("save-el");
+let count=0;
+
+function increment(){
+    
+    count+=1;
+    countEl.textContent=count
+}
+
+function save(){
+    let countstr=count +" - ";
+    saveEl.textContent+=countstr
+    countEl.textContent=0
+    count=0
+}
